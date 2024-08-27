a0c57dee6f8b5e9033bb9bf354e42ba22ee4d23b
Jürgen Sting-Lamadé <113353893+MelanargiaGalathea@users.noreply.github.com>
2024-08-27 21:04:59 +0200

    Add: Klasse AudioTest im AudioModul. Ruft eine statische Methode aus MidiPlay auf. Über die Methode können Klänge aus der Java-Midi-Bibliothek aufgerufen werden.

--- src/main/java/com/musicapp/util/MidiPlay.java
+++ src/main/java/com/musicapp/util/MidiPlay.java
@@ -5,14 +5,10 @@ import javax.sound.midi.MidiUnavailableException;
 
     public class MidiPlay extends MidiUtils {
 
-        int instrumentNr = 0;
-
         public MidiPlay() throws MidiUnavailableException {
         }
 
         public static void main(String[] args) {
-
-
 
         }
 }
